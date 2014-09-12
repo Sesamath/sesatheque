70dd1f150fc968e029cc1a80f51b46a7600830ed
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2014-09-11 19:31:21 -0500

    aj des json pour les arbres labomep

---
+++ gulptasks/jsonPost.js
@@ -0,0 +1,83 @@
+/**
+ * Ce script prend un fichier json en argument et le poste vers l'api
+ * --arbre fichier.json envoie vers /api/arbre
+ * --arbreFull fichier.json envoie vers /api/arbre?populate=1 (ira chercher les enfants pour remplacer les références)
+ * --ressource fichier.json envoie vers /api/ressource
+ */
+'use strict';
+
+// var _ = require('underscore')._
+var request = require('request')
+var moment = require('moment')
+var fs = require('fs')
+
+// conf de l'appli
+var serverConf = require('../_private/config');
+var port = serverConf.server && serverConf.server.port || 3000;
+var prefix = 'http://localhost:' +port
+
+/**
+ * Écrit en console avec le moment en préfixe
+ * @param msg
+ */
+function log(msg, objToDump) {
+  var prefix = '[' +moment().format('HH:mm:ss.SSS') +'] '
+  console.log(prefix + msg)
+  if (objToDump) console.log(objToDump)
+}
+
+/**
+ * Ajoute une ressource dans la bibli en appelant l'api en http
+ * @param uri
+ * @param obj
+ */
+function postJson(url, obj) {
+  var options = {
+    url : url,
+    json: true,
+    content_type: 'charset=UTF-8',
+    form: obj
+  }
+  request.post(options, function (error, response, body) {
+    if (error) log('Erreur ' +error)
+    else log('La réponse ', body)
+    process.exit()
+  })
+}
+
+function usage(exitCode, message) {
+  log("Il faut appeler ce scrit avec --arbre ou --arbreFull ou --ressource, suivi d'un fichier json " +
+      "(relatif au script)")
+  if (message) log(message)
+  process.exit(exitCode || 0)
+}
+
+module.exports = function () {
+  // les 3 premiers args sont node, /path/2/gulp, importMEPS
+  var argv = process.argv.slice(3)
+  var uri
+
+  log('task ' + __filename);
+
+  switch (argv[0]) {
+    case '--arbre': uri = '/api/arbre'; break;
+    case '--arbreFull': uri = '/api/arbre?populate=1'; break;
+    case '--ressource': uri = '/api/ressource'; break;
+    default : usage(1)
+  }
+  if (argv.length < 2) usage(1)
+  var jsonFile = __dirname + '/' +argv[1]
+  if (!fs.existsSync(jsonFile)) usage(1, jsonFile +" n'existe pas")
+
+  var url = prefix + uri
+  log('post de ' +jsonFile +' vers ' +url)
+  fs.readFile(jsonFile, function(error, jsonString) {
+    try {
+      if (error) throw error
+      var objToPost = JSON.parse(jsonString)
+      postJson(url, objToPost)
+    } catch (error) {
+      usage(1, jsonFile +" ne contient pas de json valide : " +error.toString())
+    }
+  })
+}
