32b814ebbd0e809882614db6da41de92007dd7cd
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2015-07-01 11:09:22 +0200

    modif import j3p pour avoir en option l'utilisation du xml d'exo interactifs de la prod labomep

--- tasks/importJ3pViaXml.js
+++ tasks/importJ3pViaXml.js
@@ -11,6 +11,7 @@ var knex = require('knex')
 var fs = require('fs')
 var flow = require('an-flow')
 var elementtree = require('elementtree')
+var request = require('request')
 
 var common = require('./modules/common')
 // raccourcis
@@ -28,6 +29,127 @@ var kLabomep = knex(dbConfigLabomep)
 
 /** Les ids que l'on a trouvé dans bibs */
 var idsFound = []
+
+/**
+ * Passe en revue l'objet elementtree issu du xml pour en extraire les ids des tags j3p (récursif)
+ * @param {elementtree} arbre
+ * @returns {array} Les ids trouvés
+ */
+function getJ3pIds(arbre) {
+  if (arbre.n) {
+    log("parsing de la branche xml " +arbre.n)
+  }
+  var j3pIds = []
+  if (arbre._children) arbre._children.forEach(function(child) {
+    if (child.tag === 'j3p') {
+      if (child.attrib.i) {
+        j3pIds.push(child.attrib.i)
+      } else {
+        common.addError("élément " +child.tag +" sans id , n° d'ordre " +child._id)
+      }
+    } else if (child.tag === 'd') {
+      j3pIds = j3pIds.concat(getJ3pIds(child))
+    }
+  })
+
+  return j3pIds
+}
+
+/**
+ * Récupère le xml en prod
+ * @param next lui file la chaine récupérée ou une erreur
+ */
+function getOnlineXml(next) {
+  var options = {
+    url         : 'http://www.labomep.net/config/xml/exercicesInteractifs/exercices_interactifs.xml',
+    content_type: 'charset=UTF-8'
+  }
+  //log('dans getRessource ' +idComb)
+  log("On va chercher " +options.url)
+  request.get(options, function (error, response) {
+    if (error) next(error)
+    else next(response.body)
+  })
+}
+
+/**
+ * Traite chacune des ids trouvées
+ * @param {Array} idsFound
+ */
+function parseIds(idsFound) {
+  if (idsFound && idsFound.length) {
+    idsFound.forEach(function (id) {
+      flow().seq(function () {
+        var row,
+            query = "SELECT bib_id AS id, bib_titre AS titre, bib_descriptif AS descriptif," +
+                    " bib_commentaire AS commentaire, bib_xml AS graphe FROM BIBS"
+        common.setAfterAllCb(this)
+        kLabomep.raw(query + " WHERE bib_id = " + id).exec(function (error, rows) {
+          if (error) throw error
+          if (rows[0] && rows[0][0]) {
+            row = rows[0][0]
+            // faut voir si on complète avec les infos de oldBibli
+            if (!row.bib_descriptif || !row.bib_commentaire) {
+              kOldBibli.raw("SELECT description, commentaires FROM Ressource WHERE id = " + id).exec(function (error,
+                                                                                                               rows) {
+                var result
+                if (error) throw error
+                if (rows[0]) {
+                  result = rows[0][0]
+                  if (result && !row.descriptif && result.description) row.descriptif = result.description
+                  if (result && !row.commentaire && result.commentaires) row.commentaire = result.commentaires
+                }
+                else log("Pas de ressources avec " + query)
+              })
+            }
+            parseRessource(row)
+          } else log("Pas de ressources dans BIBS d'id " + id)
+        })
+
+      }).seq(function () {
+        common.displayResult()
+
+      }).catch(function (error) {
+        log('Erreur dans le flow :', error)
+      })
+    })
+  } else {
+    log("Aucune ressource j3p trouvée dans le xml")
+  }
+}
+
+/**
+ * Parse un xml (et appelle splitOrNotToSplit avec le résultat)
+ */
+function parseJ3pXml(xmlString, next) {
+  var xmlName = 'exercices_interactifs' // sans l'extension
+  try {
+    if (xmlString) {
+      log('processing xml récupéré online')
+    } else {
+      log('processing xml local ' + xmlName)
+      xmlString = fs.readFileSync(__dirname +'/arbresXml/' +xmlName +'.xml').toString()
+    }
+    var arbre = elementtree.parse(xmlString)
+    if (!arbre._root) throw new Error("arbre sans racine")
+    if (!arbre._root._children || !arbre._root._children.length) throw new Error("arbre vide")
+    // si l'arbre n'a qu'un fils qui est un tag d avec enfants c'est lui qu'on prend comme racine
+    if (arbre._root._children.length === 1 && arbre._root._children[0].tag === 'd') {
+      if (arbre._root._children[0]._children.length) {
+        arbre = arbre._root._children[0]
+      } else throw new Error("arbre avec un seul dossier vide")
+    } else {
+      arbre = arbre._root
+    }
+    //log(JSON.stringify(arbre))
+
+    next(getJ3pIds(arbre))
+
+  } catch (error) {
+    common.addError(xmlName, "le parsing du xml " +xmlName +" a planté : " +error.toString())
+    process.exit()
+  }
+}
 
 /**
  * Convertit un recordset en objet Ressource que l'on pourra poster à l'api
@@ -62,59 +184,6 @@ function parseRessource(row) {
   common.addCatExoInteractif(ressource)
   //log('qui donne', ressource)
   common.pushRessource(ressource)
-}
-
-/**
- * Parse un xml (et appelle splitOrNotToSplit avec le résultat)
- */
-function parseJ3pXml() {
-  var xmlName = 'exercices_interactifs' // sans l'extension
-  log('processing xml ' + xmlName)
-  try {
-    var xmlString = fs.readFileSync(__dirname +'/arbresXml/' +xmlName +'.xml').toString()
-    var arbre = elementtree.parse(xmlString)
-    if (!arbre._root) throw new Error("arbre sans racine")
-    if (!arbre._root._children || !arbre._root._children.length) throw new Error("arbre vide")
-    // si l'arbre n'a qu'un fils qui est un tag d avec enfants c'est lui qu'on prend comme racine
-    if (arbre._root._children.length === 1 && arbre._root._children[0].tag === 'd') {
-      if (arbre._root._children[0]._children.length) {
-        arbre = arbre._root._children[0]
-      } else throw new Error("arbre avec un seul dossier vide")
-    } else {
-      arbre = arbre._root
-    }
-    //log(JSON.stringify(arbre))
-
-    return getJ3pIds(arbre)
-
-  } catch (error) {
-    common.addError(xmlName, "le parsing du xml " +xmlName +" a planté : " +error.toString())
-  }
-}
-
-/**
- * Passe en revue l'objet elementtree issu du xml pour en extraire les ids des tags j3p (récursif)
- * @param {elementtree} arbre
- * @returns {array} Les ids trouvés
- */
-function getJ3pIds(arbre) {
-  if (arbre.n) {
-    log("parsing de la branche xml " +arbre.n)
-  }
-  var j3pIds = []
-  if (arbre._children) arbre._children.forEach(function(child) {
-    if (child.tag === 'j3p') {
-      if (child.attrib.i) {
-        j3pIds.push(child.attrib.i)
-      } else {
-        common.addError("élément " +child.tag +" sans id , n° d'ordre " +child._id)
-      }
-    } else if (child.tag === 'd') {
-      j3pIds = j3pIds.concat(getJ3pIds(child))
-    }
-  })
-
-  return j3pIds
 }
 
 /**
@@ -158,8 +227,10 @@ function purgeJ3pAndExit() {
 
 log('task ' + __filename)
 
-// les 3 premiers args sont node, /path/2/gulp, nomDeLaTache
-var argv = process.argv.slice(3)
+
+
+// les 3 premiers args sont node, nomDeLaTache
+var argv = process.argv.slice(2)
 if (argv[0] === '--purge') {
   purgeJ3pAndExit()
 } else {
@@ -173,46 +244,14 @@ if (argv[0] === '--purge') {
 
   if (argv[0] === '--id') {
     idsFound = argv[1].split(',')
-  } else {
-    // on parse le xml
-    idsFound = parseJ3pXml()
-  }
-  if (idsFound) {
-    idsFound.forEach(function (id) {
-      flow().seq(function () {
-        var row,
-            query = "SELECT bib_id AS id, bib_titre AS titre, bib_descriptif AS descriptif," +
-                " bib_commentaire AS commentaire, bib_xml AS graphe FROM BIBS"
-        common.setAfterAllCb(this)
-        kLabomep.raw(query +" WHERE bib_id = " + id).exec(function (error, rows) {
-          if (error) throw error
-          if (rows[0] && rows[0][0]) {
-            row = rows[0][0]
-            // faut voir si on complète avec les infos de oldBibli
-            if (!row.bib_descriptif || !row.bib_commentaire) {
-              kOldBibli.raw("SELECT description, commentaires FROM Ressource WHERE id = " + id).exec(function (error, rows) {
-                var result
-                if (error) throw error
-                if (rows[0]) {
-                  result = rows[0][0]
-                  if (result && !row.descriptif && result.description) row.descriptif = result.description
-                  if (result && !row.commentaire && result.commentaires) row.commentaire = result.commentaires
-                }
-                else log("Pas de ressources avec " + query)
-              })
-            }
-            parseRessource(row)
-          } else log("Pas de ressources dans BIBS d'id " + id)
-        })
-
-      }).seq(function () {
-        common.displayResult()
-
-      }).catch(function (error) {
-        log('Erreur dans le flow :', error)
-      })
+  } else if (argv[0] === '--prod') {
+    // on va chercher le xml online
+    getOnlineXml(function (xml) {
+      if (xml && !xml.stack) parseJ3pXml(xml, parseIds)
+      else log(xml)
     })
   } else {
-    log("Aucune ressource j3p trouvée dans le xml")
+    // on parse le xml local
+    parseJ3pXml(null, parseIds)
   }
 }
