2a7563e4cee7b89fdc99abd1fe2187e54c6ee2c7
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2015-10-15 17:42:01 +0200

    plugin ecjs quasi bon

--- construct/ressource/public/plugins/ecjs/ecjs.js
+++ construct/ressource/public/plugins/ecjs/ecjs.js
@@ -41,10 +41,7 @@ try {
     // raccourcis, si ça plante le catch gère
     var S = window.sesamath;
 
-    // charger_options et enregistrer_score exportées dans le dom global par display
-
     /**
-     * Affiche la ressource dans l'élément d'id mepRess
      * inspiré de http://calculatice.ac-lille.fr/calculatice/bibliotheque/javascript/api/
      * @memberOf ecjs
      * @param {Ressource}      ressource  L'objet ressource
@@ -52,60 +49,116 @@ try {
      * @param {errorCallback}  next       La fct à appeler quand le swf sera chargé
      */
     ecjs.display = function (ressource, options, next) {
+      // vérifs de base
+      if (!options.container) throw new Error("Paramétrage manquant (conteneur)");
+      if (!ressource.parametres.fichierjs) throw new Error("Paramétrage manquant (nom de l'exercice à lancer)");
+
       // pour utiliser le serveur de calculatice mettre http://calculatice.ac-lille.fr/calculatice/bibliotheque/javascript
       var ecjsBase = S.getURLParameter("ecjsBase") || options.ecjsBase || "http://ressources.sesamath.net/replication_calculatice/javascript";
-      // On réinitialise le conteneur
-      S.empty(options.container);
-      // une fct pour récupérer les résultats
-      window.enregistrer_score = function (datasCalculatice) {
-        if (options && options.resultatCallback) {
-          S.log("résultats reçus", datasCalculatice);
-          options.resultatCallback({reponse: datasCalculatice});
-        }
-      };
 
       // d'après {ecjsBase}/api/clc-api.main.js
       // celui-là détruit notre style et semble ne rien apporter dans les exos
       //"http://calculatice.ac-lille.fr/calculatice/squelettes/css/clear.css",
       S.addCss(ecjsBase + "/lib-externes/jquery/css/start/jquery-ui-1.10.4.custom.min.css");
       S.addCss(ecjsBase + "/clc/css/clc.css");
-      require([ecjsBase + "/lib-externes/jquery.all.min.js"], function () {
-        // on a toujours en console ReferenceError: Raphael is not defined à son exécution
-        //S.addElement(options.container, 'script', {type:"application/javascript", src:ecjsBase + "/lib-externes/raphael.all.min.js"});
-        // apparemment Raphael veut que require soit undefined :-/
-        require([
-            ecjsBase + "/lib-externes/modernizr.custom.59181.js",
-            ecjsBase + "/lib-externes/svgjs.all.min.js",
-            ecjsBase + "/lib-externes/raphael.all.min.js",
-            ecjsBase + "/lib-externes/big.all.min.js",
-            ecjsBase + "/clc/js/clc.dev.1.js"
-        ], function () {
-          /*global CLC, $*/
-          var $container = $(options.container);
-          // les options et le nom de l'exo
-          var optionsClc = ressource.parametres.options || "default";
-          var nomExo = ressource.parametres.fichierjs;
-          var cheminExo = ecjsBase + "/exercices/";
+      require(["head_load"], function () {
+        head.ready(function () {
+          // on attend que tout soit fini pour virer require que Raphael et Big ne supportent pas
+          // on espère que plus personne n'en aura besoin
+          require = undefined;
+          define = undefined;
+          // Cf /home/sesamath/bin/fetchCalculatice.sh qui tourne sur le serveur web ressources.sesamath.net
+          // pour la liste des js concaténés dans scripts.js
+          head.load(ecjsBase + "/scripts.js", function () {
+            // on vérifie que l'on a tous les objets globaux souhaités
+            var glob = ["Modernizr", "$", "SVG", "Raphael", "Big", "createjs", "CLC"];
+            var prop, i;
+            for (i = 0; i < glob.length; i++) {
+              prop = glob[i];
+              if (typeof window[prop] === "undefined") throw new Error("Problème de chargement, " +prop +" n'existe pas");
+            }
 
-          // cree un exo de maniere asynchrone
-          var reqExo  = CLC.loadExo(cheminExo+nomExo,optionsClc);
+            /*global CLC, $*/
+            function envoyerScoreExoJs(event, data){
+              S.log("résultats reçus du js calculatice", data);
+              resultatSent = true; // même si ça plante, pas la peine de recommencer au unload
+              var dataToSend = {
+                fin : true,
+                date: startDate,
+                duree : data.duree
+              };
+              if (data.total > 0) {
+                var score = parseInt(data.score, 10) || 0;
+                dataToSend.score = score / data.total;
+                dataToSend.reponse = score +" sur " +data.total;
+              } else {
+                dataToSend.reponse = "score indéterminé";
+              }
+              if (options && options.resultatCallback) {
+                options.resultatCallback(dataToSend);
+              }
+            }
 
-          // quand l'exo est pret on le met dans la div et on l'affiche avec une dialog jquery
-          // Attention l'id de la div est ici "cntExo", changer si besoin.
-          reqExo.done(function(exercice){
-            S.log("exo clc", exercice);
-            $container.width(775);
-            $container.html(exercice);
-            if (!options.isFormateur) {
-              //$container.ready(function () {
-              $(function () {
-                $("#display button.parametrer").hide();
-                S.log("paramétrage caché");
+            // On réinitialise le conteneur
+            var container = options.container;
+            S.empty(container);
+            var exoClc = S.addElement(container, 'div', {id:"exoclc", style:{margin:"0 auto", width:"735px"}});
+            var footer = S.addElement(container, 'p', {style:{"text-align":"right", margin:"0 auto", width:"735px"}}, "Exercice original provenant du site ");
+            S.addElement(footer, 'a', {href:"http://calculatice.ac-lille.fr/", target:"_blank"}, "Calcul@tice");
+            var $exoClc = $(exoClc);
+            // les options et le nom de l'exo
+            var optionsClc = ressource.parametres.options || {};
+            optionsClc.parametrable = !!options.isFormateur;
+            var nomExo = ressource.parametres.fichierjs;
+            var cheminExo = ecjsBase + "/exercices/";
+            var exoLoaded = false;
+            var startDate;
+            var resultatSent = false;
+
+            // si ça intéresse l'appelant et que le chargement est KO on finira par le dire après 10s
+            if (next && typeof next === "function") {
+              setTimeout(function () {
+                if (!exoLoaded) next(new Error("Exercice calculatice toujours pas chargé après 10s"));
+              }, 10000); // on laisse 10s avant d'envoyer une erreur de chargement
+            }
+            // on envoie un score vide au unload si rien n'a été envoyé avant
+            if (options && options.resultatCallback) {
+              // on ajoute un envoi au unload si rien n'a été envoyé avant
+              window.addEventListener('unload', function () {
+                S.log("unload ecjs");
+                if (startDate && !resultatSent) {
+                  envoyerScoreExoJs(null, {
+                    date : startDate,
+                    duree : Math.floor(((new Date()).getTime() - startDate.getTime()) / 1000),
+                    score : 0,
+                    reponse : "Aucune réponse",
+                    fin : true
+                  });
+                }
               });
             }
+            // cree un exo de maniere asynchrone
+            var reqExo  = CLC.loadExo(cheminExo + nomExo, optionsClc);
+
+            // quand l'exo est pret on le met dans la div et on l'affiche avec une dialog jquery
+            // Attention l'id de la div est ici "cntExo", changer si besoin.
+            reqExo.done(function(exercice){
+              S.log("exo clc", exercice);
+              $exoClc.html(exercice);
+              startDate = new Date();
+              // le 2e arg se retrouve dans event.data (event 1er arg passé à la callback)
+              // pour la liste des événements, chercher "publier" dans les sources calculatice
+              // on a validationQuestion, validationOption, finExercice
+              // l'evt est lancé sur
+              // pour finExercice on récupère {idExo, numExo, score {int, nb de bonnes réponses}, total {int, nb total de questions}, duree {int, en secondes}}
+              exercice.on("finExercice", null, envoyerScoreExoJs);
+              if (next && typeof next === "function") {
+                exoLoaded = true;
+                next();
+              }
+            });
           });
         });
-
       });
     };
 
