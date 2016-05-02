6ab2b3712077905e470de8ca569ade091852851f
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2016-05-02 19:53:30 +0200

    bugfix

--- app/srcClient/edit/import.js
+++ app/srcClient/edit/import.js
@@ -72,7 +72,7 @@ function doImport (url) {
     // console.log('retour externalClone', error, data)
     if (error) page.addError(error)
     else if (data && data.error) page.addError(data.error)
-    else if (data && data.oid) window.location = '/ressource/modifier/' + data.oid
+    else if (data && (data.oid || data.ref)) window.location = '/ressource/modifier/' + data.oid
     else page.addError('L’import a échoué, vérifier l’url fournie')
   })
 }
