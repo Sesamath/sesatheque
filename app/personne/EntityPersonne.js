7b4f89f5bbce32ce60803c7567385592f97d15eb
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2017-09-15 01:04:31 +0200

    retag 1.0.12 | upgrade sesaeditgraphe#0.1.21, sesajstools#1.0.14, lassi#2.1.15

--- app/personne/EntityPersonne.js
+++ app/personne/EntityPersonne.js
@@ -96,15 +96,15 @@ module.exports = function (EntityPersonne, $cachePersonne) {
   })
 
   EntityPersonne
-      .defineIndex('pid', 'string')
-      .defineIndex('nom', 'string')
-      .defineIndex('email', 'string')
-      // par défaut, la valeur de l'index est la valeur du champ, mais on peut fournir
-      // une callback qui renvoie la valeur (ou un tableau de valeurs)
-      .defineIndex('roles', 'string', function () {
-        log.debug('roles de ' + this.oid, sjtObj.truePropertiesList(this.roles))
-        return sjtObj.truePropertiesList(this.roles)
-      })
-      .defineIndex('groupesMembre', 'string')
-      .defineIndex('groupesSuivis', 'string')
+    .defineIndex('pid', 'string')
+    .defineIndex('nom', 'string')
+    .defineIndex('email', 'string')
+    // par défaut, la valeur de l'index est la valeur du champ, mais on peut fournir
+    // une callback qui renvoie la valeur (ou un tableau de valeurs)
+    .defineIndex('roles', 'string', function () {
+      log.debug('roles de ' + this.oid, sjtObj.truePropertiesList(this.roles))
+      return sjtObj.truePropertiesList(this.roles)
+    })
+    .defineIndex('groupesMembre', 'string')
+    .defineIndex('groupesSuivis', 'string')
 }
