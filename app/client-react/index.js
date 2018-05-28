5af9a47ee6761ba194e492a909fb54512aa7c455
Manuel Baclet <manuel.baclet@thetribe.io>
2018-05-28 15:52:47 +0200

    Small changes in react bootstrapping

--- app/client-react/index.js
+++ app/client-react/index.js
@@ -1,8 +1,5 @@
-import App from "./App.jsx";
-import React from 'react'
-import ReactDOM from 'react-dom'
+import {createElement} from 'react'
+import {render} from 'react-dom'
+import App from './App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+render(createElement(App), document.getElementById('root'))
