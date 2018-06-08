23f0d3886bf1e030f3af31719bdce1103917ce00
Manuel Baclet <manuel.baclet@thetribe.io>
2018-06-08 16:38:31 +0200

    Map ressource editor to ressource type

--- app/client-react/components/ResourceForm.js
+++ app/client-react/components/ResourceForm.js
@@ -3,27 +3,38 @@ import React from 'react'
 import {flowRight} from 'lodash'
 import {reduxForm} from 'redux-form'
 import MetaForm from './MetaForm'
+import EditorIep from './EditorIep'
 import EditorJ3p from './EditorJ3p'
 import resourceLoader from './resourceLoader'
 import ShowError from './ShowError'
 
+const typeToComponent = {
+  iep: EditorIep,
+  j3p: EditorJ3p
+}
+
 const ResourceForm = ({
+  initialValues: {type},
   handleSubmit,
   pristine,
   change,
   submitting,
   saveError
-}) => (
-  <form onSubmit={handleSubmit}>
-    <MetaForm />
-    <hr />
-    <EditorJ3p change={change} />
-    <div className="buttons-area">
-      <button type="submit" className="btn--primary" disabled={pristine || submitting}>Enregistrer</button>
-    </div>
-    <ShowError error={saveError} />
-  </form>
-)
+}) => {
+  const Editor = typeToComponent[type]
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <MetaForm />
+      <hr />
+      <Editor change={change} />
+      <div className="buttons-area">
+        <button type="submit" className="btn--primary" disabled={pristine || submitting}>Enregistrer</button>
+      </div>
+      <ShowError error={saveError} />
+    </form>
+  )
+}
 
 ResourceForm.propTypes = {
   handleSubmit: PropTypes.func,
