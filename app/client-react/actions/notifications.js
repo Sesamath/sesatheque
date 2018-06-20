6f124fc0656f3a783872f97cfece79b2c7c72c1e
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2018-06-20 13:58:57 +0200

    ajout commentaires

--- app/client-react/actions/notifications.js
+++ app/client-react/actions/notifications.js
@@ -3,6 +3,23 @@ let counter = 0
 const displayDuration = 10000
 const additionalDisplayDuration = 2000
 
+/**
+ * @typedef ActionAddNotification
+ * @type Object
+ * @property {string} type vaut toujours 'ADD_NOTIFICATION'
+ * @property {string} message
+ * @property {string} level
+ * @property {number} id
+ * @property {number} to id du timer removeNotification (si level === error) ou null
+ */
+
+/**
+ * Retourne l'actionCreator pour ADD_NOTIFICATION
+ * @param {object} notification
+ * @param {string} notification.message
+ * @param {string} notification.level
+ * @return {Function} actionCreator qui fera le dispatch de l'action {@link ActionAddNotification}
+ */
 export const addNotification = ({message, level}) => (dispatch, getState) => {
   const length = getState().notifications.length
   const id = counter++
@@ -15,6 +32,11 @@ export const addNotification = ({message, level}) => (dispatch, getState) => {
   })
 }
 
+/**
+ * Retourne l'action REMOVE_NOTIFICATION
+ * @param {number} id
+ * @return {{type: string, id: number}}
+ */
 export const removeNotification = (id) => ({
   type: 'REMOVE_NOTIFICATION',
   id
