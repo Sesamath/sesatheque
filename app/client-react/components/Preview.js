fbefd3e05e6d6563f03ae9d6ae5c4aa0a905833d
Manuel Baclet <manuel.baclet@thetribe.io>
2018-06-21 10:12:27 +0200

    Add support for ressource preview
    This reverts commit ec47f72daa701aca052b26bbeb030dc4ad4de47a.

---
+++ app/client-react/components/Preview.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import React, {Fragment} from 'react'
+import NavMenu from './NavMenu'
+import resourceLoader from './resourceLoader'
+
+const Preview = ({
+  initialValues: {oid: ressourceOid, titre}
+}) => (
+  <Fragment>
+    <h1 className="fl">Aperçu de la ressource {titre}</h1>
+    <NavMenu ressourceOid={ressourceOid} />
+    <iframe src={`/ressource/voir/${ressourceOid}`} />
+  </Fragment>
+)
+
+Preview.propTypes = {
+  initialValues: PropTypes.shape({
+    oid: PropTypes.string,
+    titre: PropTypes.string
+  })
+}
+
+export default resourceLoader(Preview)
