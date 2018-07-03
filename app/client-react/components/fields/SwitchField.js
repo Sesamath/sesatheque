999ca7618fc264a76953069f84dd6bc6ceb889cf
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2018-07-03 08:54:26 +0200

    checked viré, c'est redux-form qui doit mapper une valeur

--- app/client-react/components/fields/SwitchField.js
+++ app/client-react/components/fields/SwitchField.js
@@ -2,21 +2,19 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import {Field} from 'redux-form'
 
-const SwitchField = ({checked, className, label, name}) => (
+const SwitchField = ({className, label, name}) => (
   <label className={className}>
     {label}
     <Field
       name={name}
       component="input"
       type="checkbox"
-      checked={checked}
       className="switch"
     />
   </label>
 )
 
 SwitchField.propTypes = {
-  checked: PropTypes.bool,
   className: PropTypes.string,
   label: PropTypes.string,
   name: PropTypes.string
