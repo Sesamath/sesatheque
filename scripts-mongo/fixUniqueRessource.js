fecff51524c46c7a8af5bf1497c7bd84cfb0a3cd
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2018-07-27 09:39:17 +0200

    rectif fix cle, aj fix rid

--- scripts-mongo/fixUniqueRessource.js
+++ scripts-mongo/fixUniqueRessource.js
@@ -1,16 +1,18 @@
-/* global db print printjson ObjectId */
+/* global db print ObjectId */
 
 print('Supression des clés vides')
-let result = db.EntityRessource.updateMany({cle: ''}, {$unset: {cle: '', _data: {cle: ''}}})
+// https://docs.mongodb.com/manual/reference/method/db.collection.updateMany/
+let result = db.EntityRessource.updateMany({cle: ''}, {$unset: {cle: '', '_data.cle': ''}})
 print(`${result.modifiedCount} propriétés cle supprimées (elles étaient vides)`)
 
 print('suppression des clés des ressources publiques')
-result = db.EntityRessource.updateMany({publie: true, restriction: 0}, {$unset: {cle: '', _data: {cle: ''}}})
+result = db.EntityRessource.updateMany({publie: true, restriction: 0}, {$unset: {cle: '', '_data.cle': ''}})
 print(`${result.modifiedCount} propriétés cle supprimées (ressources publiques)`)
 
 // les doublons de clés
 let nb = 0
-let agCursor = db.EntityRessource.aggregate([
+// https://docs.mongodb.com/manual/reference/method/db.collection.aggregate/
+db.EntityRessource.aggregate([
   {
     $group: {
       _id: {cle: '$cle'},
@@ -19,9 +21,7 @@ let agCursor = db.EntityRessource.aggregate([
   }, {
     $match: {count: {$gt: 1}}
   }
-])
-while (agCursor.hasNext()) {
-  const result = agCursor.next()
+]).forEach(result => {
   const cle = result._id.cle
   if (cle !== null) {
     print(`${result.count} doublons pour la clé ${cle}`)
@@ -30,14 +30,35 @@ while (agCursor.hasNext()) {
     const rCursor = db.EntityRessource.find({cle})
     while (rCursor.hasNext()) {
       const ressource = rCursor.next()
+      // https://docs.mongodb.com/manual/reference/method/ObjectId.valueOf/#ObjectId.valueOf
       ressource.cle = ObjectId().valueOf()
       print(`pour ${ressource._id} on met la clé ${ressource.cle}`)
       ressource._data.cle = ressource.cle
       db.EntityRessource.save(ressource)
     }
   }
-}
+})
 if (nb === 0) print('Aucun doublon de clé')
 
 // il faut aller remplir tous les rid vides…
-// on doit récupérer notre baseId, mais on a pas de require…
+// on doit récupérer notre baseId, mais on a pas de require… on va regarder en base
+let cursor = db.EntityRessource.find()
+let baseId
+while (cursor.hasNext() && !baseId) {
+  const r = cursor.next()
+  if (r.rid) {
+    baseId = r.rid.substr(0, r.rid.indexOf('/'))
+    print(`On a trouvé baseId=${baseId}`)
+  }
+}
+nb = 0
+const setRid = (r) => {
+  nb++
+  const rid = `${baseId}/${r.oid}`
+  delete r.rid
+  r._data.rid = rid
+  db.EntityRessource.save(r)
+}
+db.EntityRessource.find({rid: {$in: ['', null]}}).forEach(setRid)
+db.EntityRessource.find({'_data.rid': {$in: ['', null]}}).forEach(setRid)
+print(`${nb} ressources sans rid corrigées`)
