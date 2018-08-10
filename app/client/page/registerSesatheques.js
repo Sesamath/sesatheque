4edef0c71a3718f41b155b20f4b88d6636f992bf
Manuel Baclet <manuel.baclet@thetribe.io>
2018-08-10 14:11:22 +0200

    Lint fix

--- app/client/page/registerSesatheques.js
+++ app/client/page/registerSesatheques.js
@@ -4,5 +4,5 @@ import {addSesatheque} from 'sesatheque-client/src/sesatheques'
 // init de cette sesatheque et des autres pour sesatheque-client
 addSesatheque(baseId, baseUrl)
 if (sesatheques.length) {
-    sesatheques.forEach(({baseId, baseUrl}) => addSesatheque(baseId, baseUrl))
+  sesatheques.forEach(({baseId, baseUrl}) => addSesatheque(baseId, baseUrl))
 }
