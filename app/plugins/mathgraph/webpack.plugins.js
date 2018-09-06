5e2ce228ef5e8776cdf25eb119764ddecc725247
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2018-09-06 15:22:07 +0200

    image save ajoutée

--- app/plugins/mathgraph/webpack.plugins.js
+++ app/plugins/mathgraph/webpack.plugins.js
@@ -2,12 +2,13 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
 
 const plugins = [
-  new CopyWebpackPlugin([
-    {
-      from: path.resolve(__dirname, 'public/spinner.gif'),
-      to: 'plugins/mathgraph/spinner.gif'
-    }
-  ])
+  new CopyWebpackPlugin([{
+    from: path.resolve(__dirname, 'public/spinner.gif'),
+    to: 'plugins/mathgraph/spinner.gif'
+  }, {
+    from: path.resolve(__dirname, 'public/outilSave.png'),
+    to: 'plugins/mathgraph/outilSave.png'
+  }])
 ]
 
 module.exports = plugins
