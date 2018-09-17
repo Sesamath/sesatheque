c4b61669ccf3f9105698f929cef80945c35a4384
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2018-09-17 09:39:30 +0200

    fix pb déclaration

--- app/plugins/arbre/validate.js
+++ app/plugins/arbre/validate.js
@@ -1,3 +1,9 @@
+/**
+ * Valide un arbre (ajoutera éventuellement une string dans errors.enfants)
+ * @param {Ressource} arbre
+ * @param {Object} errors
+ * @param {string} errors.enfants L'erreur éventuelle qui sera ajoutée en cas de pb de validation
+ */
 const validate = (arbre, errors) => {
   let children = arbre.enfants
   if (typeof children === 'string') {
@@ -37,9 +43,11 @@ const validate = (arbre, errors) => {
       }
     })
   }
+
   treeIterate(children)
+
   if (error) {
-    console.error(Error(error), enfants)
+    console.error(Error(error), arbre.enfants)
     errors.enfants = error
   }
 }
