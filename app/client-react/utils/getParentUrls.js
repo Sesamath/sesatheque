e5b43af0d46cd01c65c56fc4bc04d0e889305b35
Manuel Baclet <manuel.baclet@thetribe.io>
2018-09-21 15:58:41 +0200

    Add iframe hierarchy list to bugsnag reports

---
+++ app/client-react/utils/getParentUrls.js
@@ -0,0 +1,19 @@
+const getParentUrls = () => {
+  const frames = [window.location.href]
+  let frame = window
+  // frame.parent devrait toujours exister, mais avec les navigateurs exotiques on sait jamais
+  // et on limite à 5 pour éviter une boucle infinie
+  // (ou plus malin, s'arrêter si l'url qu'on ajouterait est déjà la dernière du tableau)
+  let i = 0
+  try {
+    while (frame.parent && frame.parent !== frame && i < 5) {
+      frame = frame.parent
+      i++
+      frames.push(frame.location.href)
+    }
+  } catch (_) {}
+
+  return frames
+}
+
+export default getParentUrls
