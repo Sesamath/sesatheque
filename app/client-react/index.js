c5698ce838e2fb9d52d2668896251f9892d2232f
Manuel Baclet <manuel.baclet@thetribe.io>
2018-09-26 11:22:31 +0200

    Use another URL polyfill (previous is buggy with Safari)

--- app/client-react/index.js
+++ app/client-react/index.js
@@ -1,12 +1,14 @@
 // inclusion des polyfills:
 import '@babel/polyfill'
 import 'whatwg-fetch'
-import 'url-polyfill'
 
 import {createElement} from 'react'
 import {render} from 'react-dom'
 import App from './App'
 import checkBrowser from './utils/checkBrowser'
+
+// inclusion du polyfill URL:
+require('js-polyfills/url')
 
 checkBrowser()
 
