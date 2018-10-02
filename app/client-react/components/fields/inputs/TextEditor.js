bbc82ad21982df745149a8d3b2738804fbb32d50
Manuel Baclet <manuel.baclet@thetribe.io>
2018-10-02 06:48:24 +0200

    We expect valid JSON here and we let bugsnag reports problems

--- app/client-react/components/fields/inputs/TextEditor.js
+++ app/client-react/components/fields/inputs/TextEditor.js
@@ -18,12 +18,7 @@ import './TextEditor.scss'
 function formatJSON (value) {
   if (typeof value === 'string') return value
 
-  try {
-    return JSON.stringify(value, null, 2)
-  } catch (error) {
-    console.error(error)
-    return '{}'
-  }
+  return JSON.stringify(value, null, 2)
 }
 
 const TextEditor = ({ input: { value, onChange, onBlur, onFocus }, onValidate, mode }) => (
