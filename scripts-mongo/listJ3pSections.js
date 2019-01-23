ed0c860ccc0459e1c98b31aa4125633333b93be9
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2019-01-23 10:49:03 +0100

    retag 1.4.10 | màj @sesatheque-plugins/j3p 1.0.17

--- scripts-mongo/listJ3pSections.js
+++ scripts-mongo/listJ3pSections.js
@@ -3,7 +3,7 @@
 // pour compter par section faire ensuite
 // awk '{print $1}' < j3p.list |sort|uniq -c
 // ou bien
-// awk '{print $1}' < j3p.list |sort|uniq -c|sed -e 's/^ *//; s/ /\t/'|sort -n -k1
+// awk '{print $1}' < j3p.list |sort|uniq -c|sed -e 's/^ *//; s/ /\t/'|sort -nr -k1
 
 /* global db print */
 db.EntityRessource.find({'_data.type': 'j3p'}).sort({dateCreation: 1}).forEach(r => {
